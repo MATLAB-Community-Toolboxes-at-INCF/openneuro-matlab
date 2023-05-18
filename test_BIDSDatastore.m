702637bc1181ddeb91fe2742ad695cf6bc35d854
Alex Estrada <87288871+stradaa@users.noreply.github.com>
2023-05-18 10:35:26 -0700

    Delete test_BIDSDatastore.m

--- test_BIDSDatastore.m
+++
@@ -1,8 +0,0 @@
-%% Testing BIDSDatastore 
-bucket = "openneuro.org";
-
-% Select dataset
-% ID = "ds004560";                % MR (https://openneuro.org/datasets/ds004560/versions/1.0.0)
-ID = "ds004551";                % iEEG (https://openneuro.org/datasets/ds004551/versions/1.0.5)
-ID_yijay = "ds004529";  % work with folder trees
-ds = BIDSDatastore(bucket, ID);
